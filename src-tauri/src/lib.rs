cb34d461fe0adfb4dc5f1a0788ec3d7dab80c712
Liam Wahlberg <liam.wahlberg07@gmail.com>
2024-09-23 16:17:17 +0200

    feat: persist window state

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 use tauri::Manager;
+use tauri_plugin_window_state::{AppHandleExt, StateFlags, WindowExt};
 
 #[cfg(target_os = "macos")]
 use tauri_plugin_decorum::WebviewWindowExt;
@@ -15,6 +16,8 @@ pub fn run() {
 
             for window_name in ["splash-screen", "main-launcher"] {
                 if let Some(window) = app.get_webview_window(window_name) {
+                    let _ = window.restore_state(StateFlags::all());
+
                     utils::window_ext::apply_window_effects(&window);
 
                     #[cfg(target_os = "macos")]
@@ -29,6 +32,9 @@ pub fn run() {
         })
         .on_window_event(|window, event| match event {
             tauri::WindowEvent::CloseRequested { api, .. } => {
+                let app = window.app_handle();
+                let _ = app.save_window_state(StateFlags::all());
+
                 if window.label() == "main-launcher" {
                     window.hide().unwrap();
                     api.prevent_close();
@@ -42,6 +48,7 @@ pub fn run() {
                 window.set_focus().unwrap();
             }
         }))
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_system_info::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_os::init())
