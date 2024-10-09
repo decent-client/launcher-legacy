5abd25fcd73ee9ab951ac66178aa06b4412d817f
Liam Wahlberg <liam.wahlberg07@gmail.com>
2024-10-09 16:07:30 +0200

    chore: splash screen gretting

--- app/routes/splash-screen.tsx
+++ app/routes/splash-screen.tsx
@@ -1,6 +1,8 @@
 import { invoke } from "@tauri-apps/api/core";
+import { AnimatePresence, motion } from "framer-motion";
 import { Loader2 } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
+import { useSelectedAccount } from "~/lib/providers/account";
 import type { Handle } from "~/lib/types/handle";
 import { sleep } from "~/lib/utils";
 
@@ -32,8 +34,10 @@ const loadingSubtitles = [
 ];
 
 export default function SplashScreen() {
+	const { selectedAccount } = useSelectedAccount();
 	const [currentSubtitleIndex, setCurrentSubtitleIndex] = useState(0);
 	const [isSlowConnection, setIsSlowConnection] = useState(false);
+	const [showGreet, setShowGreet] = useState(false);
 	const intervalRef = useRef<NodeJS.Timeout>();
 
 	useEffect(() => {
@@ -61,42 +65,65 @@ export default function SplashScreen() {
 		};
 	}, []);
 
+	async function setupWindows() {
+		await sleep(5000);
+
+		if (selectedAccount) {
+			setShowGreet(true);
+			await sleep(2000);
+		}
+
+		await invoke("setup_windows");
+	}
+
 	return (
 		<div
-			className="pointer-events-none relative grid h-screen place-items-center text-center font-sans"
+			className="relative grid h-screen place-items-center text-center font-sans"
 			data-tauri-drag-region
 		>
-			<span className="flex flex-col items-center justify-center">
-				<h1 className="font-black text-3xl">
-					{/* <ShinySpan className="tracking-wide"> */}
-					Decent Client
-					{/* </ShinySpan> */}
-				</h1>
-				<p className="flex items-center gap-2 font-medium text-muted-foreground text-sm italic">
-					{/* <ShinySpan className="linear-mask flex items-center gap-2 text-sm font-medium italic text-zinc-500"> */}
-					{loadingSubtitles[currentSubtitleIndex]}
-					<Loader2 className="animate-spin stroke-muted-foreground" size={14} />
-					{/* </ShinySpan> */}
-				</p>
-			</span>
+			<AnimatePresence>
+				{showGreet && selectedAccount ? (
+					<motion.h2
+						className="pointer-events-none font-black text-2xl drop-shadow-md"
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ delay: 0.2 }}
+					>
+						Welcome back, {selectedAccount.username}
+					</motion.h2>
+				) : (
+					<motion.span
+						className="ointer-events-none flex flex-col items-center justify-center drop-shadow-md"
+						initial={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+					>
+						<h1 className="font-black text-3xl">Decent Client</h1>
+						<p className="flex items-center gap-2 font-medium text-muted-foreground text-sm italic">
+							{loadingSubtitles[currentSubtitleIndex]}
+							<Loader2
+								className="animate-spin stroke-muted-foreground"
+								size={14}
+							/>
+						</p>
+					</motion.span>
+				)}
+			</AnimatePresence>
 			{isSlowConnection && (
-				<p className="pointer-events-none absolute right-2 bottom-1 font-medium text-red-300 text-sm">
+				<p className="pointer-events-none absolute right-2 bottom-1 font-medium text-red-300 text-sm drop-shadow-md">
 					Slow connection?
 				</p>
 			)}
-			<img
-				className="-scale-x-100 pointer-events-none absolute bottom-4 left-3"
+			<motion.img
+				className="-scale-x-100 pointer-events-none absolute bottom-4 drop-shadow-md"
 				src={"/gifs/minecraft-bee.gif"}
 				alt="Minecraft Bee"
+				animate={{
+					left: !showGreet ? "0.75rem" : "100vw",
+					transition: { duration: 0.75, ease: "easeInOut" },
+				}}
 				width={64}
 				height={64}
 			/>
 		</div>
 	);
-}
-
-function setupWindows() {
-	if (typeof document !== "undefined") {
-		sleep(5000).then(() => invoke("setup_windows"));
-	}
 }
